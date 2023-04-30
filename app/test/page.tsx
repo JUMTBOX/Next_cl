cfe64165220fcce748eacbaabd362070f7e3c947
JUMTBOX <yjey12@naver.com>
2023-04-30 21:49:27 +0900

    ...

--- app/test/page.tsx
+++ app/test/page.tsx
@@ -12,7 +12,7 @@ export default function portfolio() {
       {projectName?.map((item, idx) => {
         return (
           <div className="portfolio" key={idx}>
-            {item}
+            TITLE: {item}
           </div>
         );
       })}
